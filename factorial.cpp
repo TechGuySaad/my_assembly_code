1e2f0f0b282d34b59f21d35d6e50838b8ce0662a
Saad Rahman <saadreh5@gmail.com>
2021-11-20 15:05:16 +0500

    made the factorial code for cpp

--- factorial.cpp
+++ factorial.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+    int x;
+    int factorial = 1;
+    cin >> x;
+
+    while(x != 0)
+    {
+        factorial = x*factorial;
+        x--;
+
+        
+
+    }
+
+    cout << factorial<<endl;
 
 }
