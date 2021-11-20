a157bcd7fe9d0b951e579c36f62fc8928d1dd7e2
Saad Rahman <saadreh5@gmail.com>
2021-11-20 15:21:40 +0500

    implemented factorial in assembly without debugging

--- fac.asm
+++ fac.asm
@@ -1,5 +1,21 @@
 [org 0x0100]
 
+jmp start
+
+factorial: dw 1
+val: dw 3
+
+
+start:
+
+mov ax, [factorial]
+mov bx, [val]
+mul bx
+mov [factorial], ax
+
+dec [val]
+jnz start
+
 
 
 mov ax , 0x4c00
